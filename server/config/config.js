26a2861bd473d26d3767d1e25cc9a1b49b2ff971
zxkronos <zxkronos@gmail.com>
2019-01-08 00:16:06 -0300

    variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:zx8090@ds151614.mlab.com:51614/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 process.env.URLDB = urlDB;
